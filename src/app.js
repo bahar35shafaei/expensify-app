45417e0a62e69b306df771bb2a7c32e2fb07752f
bahar35shafaei <bahar35shafaei@gmail.com>
2019-07-16 21:10:07 +0430

    Remove dumy expense data

--- src/app.js
+++ src/app.js
@@ -13,9 +13,7 @@ import './styles/style.scss';
 const store = configurStore();
 
 
-store.dispatch(addExpense({description : 'gaz bill', createdAt:1000}));
-store.dispatch(addExpense({description : 'water bill', amount : 4500}));
-store.dispatch(addExpense({description : 'rent', amount : 109500}));
+
 
 // store.dispatch(setTextFilter('water'));
 
@@ -24,9 +22,7 @@ store.dispatch(addExpense({description : 'rent', amount : 109500}));
 // }, 3000);
 
 const state = store.getState();
-const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
 
-console.log(visibleExpenses);
 
 const jsx = (
     <Provider store={store}>
